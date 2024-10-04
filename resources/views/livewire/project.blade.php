ed198d45de9a5e6b5189dc2d92fe848da7117507
fzrilsh <fazriloke18@gmail.com>
2024-10-04 08:51:00 +0700

    add: section tag

--- resources/views/livewire/project.blade.php
+++ resources/views/livewire/project.blade.php
@@ -4,19 +4,21 @@
         <p class="opacity-60 mb-10">I’ve worked with a variety of technologies and tools to build cool things. Here are some of the projects I’ve worked on.</p>
         <div class="grid grid-cols-1 group">
             @foreach ($projects as $project)
-            <a href="{{ $project->link }}" target="_blank" class="sm:py-5 py-10 flex flex-col sm:flex-row gap-6 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
-                <div class="h-full bg-gray-700 rounded-lg aspect-video sm:h-[150px]"> 
-                    <img src="{{ asset('public/storage/' . $project->image) }}" alt="{{ $project->title }}" class="w-full h-full object-cover rounded-lg aspect-video" width="1920" height="1080" loading="lazy" decoding="async">
-                </div>
-                <div>
-                    <div class="flex items-center gap-1 mb-2">
-                        <h2 class="font-medium">{{ $project->title }}</h2> <span class="opacity-60">· {{ date('Y', strtotime($project->created_at)) }}</span>
+            <section id="{{ $project->id }}">
+                <a href="{{ $project->link }}" target="_blank" class="sm:py-5 py-10 flex flex-col sm:flex-row gap-6 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
+                    <div class="h-full bg-gray-700 rounded-lg aspect-video sm:h-[150px]"> 
+                        <img src="{{ asset('public/storage/' . $project->image) }}" alt="{{ $project->title }}" class="w-full h-full object-cover rounded-lg aspect-video" width="1920" height="1080" loading="lazy" decoding="async">
                     </div>
-                    <div class="opacity-60">
-                        <p>{{ $project->description }}</p>
+                    <div>
+                        <div class="flex items-center gap-1 mb-2">
+                            <h2 class="font-medium">{{ $project->title }}</h2> <span class="opacity-60">· {{ date('Y', strtotime($project->created_at)) }}</span>
+                        </div>
+                        <div class="opacity-60">
+                            <p>{{ $project->description }}</p>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </section>
             @endforeach
         </div>
     </section>
