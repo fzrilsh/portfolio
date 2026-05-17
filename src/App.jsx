d1fa508bec09542db9edfce175d33f0e35e15afa
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-05-17 19:58:11 +0700

    refactor: simplify HomePage component by removing unused sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Navigation from './components/Navigation'
-import HeroSection from './components/HeroSection'
-import AboutSection from './components/AboutSection'
-import EducationSection from './components/EducationSection'
-import WorkExperienceSection from './components/WorkExperienceSection'
-import AchievementsSection from './components/AchievementsSection'
-import PortfolioSection from './components/PortfolioSection'
-import Footer from './components/Footer'
-
-function HomePage() {
-  return (
-    <div className="min-h-screen bg-black">
-      <Navigation />
-      <HeroSection />
-      <AboutSection />
-      <EducationSection />
-      <WorkExperienceSection />
-      <AchievementsSection />
-      <PortfolioSection />
-      <Footer />
-    </div>
-  )
-}
+import HomePage from './pages/Homepage'
 
 function App() {
   return (
